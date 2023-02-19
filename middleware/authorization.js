294e77dfaf1ae29eb9bed22db8b19378ec9ffe66
J-Filip <filip.jugkala@yahoo.com>
2023-02-19 14:44:57 +0100

    This is the middleware given in the challenge

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,7 +1,5 @@
 module.exports = function authorization (req, res, next) {
-  console.log(req.cookies);
-  const token = req.cookies.token;
-  if (token !== 'xyz0987654321') {
+  if (req.headers.authorization !== 'xyz0987654321') {
     return res.sendStatus(401)
   }
 
