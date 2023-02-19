3d76ba261de9def41b794993efc576f79fe37e5f
J-Filip <filip.jugkala@yahoo.com>
2023-02-19 14:39:50 +0100

    Added encoder function that will accept a string and the output will represent successive characters as character and single digit count.

--- encoder.js
+++ encoder.js
@@ -1,11 +1,5 @@
-// encoder("XXXYYCCYYYXXXXX");
-
-// Write a simple encoder() function that will accept a string and the output will represent
-// successive characters as character and single digit count.
-// For example, the string “XXXYYYYZZQXX” would be encoded as “X3Y4Z2Q1X2”. You can assume the string to be encoded has no digits and consists solely of alphabetic characters.
-
-// I asked chatGPT to do this algorithm, not suprisingly it did better but I will leave my solution here as well.
-
+// I asked chatGPT to do this algorithm, not suprisingly it did better but I will leave my solution.
+//TODO: optimize if possible
 function encoder(userInput) {
   // transform to array of letters
   let splitted = userInput.split("");
@@ -25,7 +19,6 @@ function encoder(userInput) {
     }
   });
 
-  console.log(result.join(""));
   return result.join('')
 }
 
