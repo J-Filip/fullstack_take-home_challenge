fd82fbaae54d9a96659cae154f65d13eee12c5a4
J-Filip <filip.jugkala@yahoo.com>
2023-02-19 14:47:50 +0100

    Added new endpoint that accepts a string, run that string through the encoder function and return value

--- app.js
+++ app.js
@@ -1,46 +1,27 @@
 const express = require('express')
-let bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
 
 const login = require('./login')
 const authorization = require("./middleware/authorization");
 const encoder= require("./encoder");
 
-
 const app = express()
 const port = 3000
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
-app.set("view engine", "ejs");
-
 
 // routes
-
-app.get('/', (req, res) => {
-      res.render('login.ejs')
-})
-
 app.post('/login', login)
 
-app.get("/encode", authorization, (req, res) => {
-  res.render("encode.ejs");
-});
-
-// define a new endpoint that accepts a string
-app.post("/encode", (req, res) => {
-  const encodedStr = encoder(req.body.userInput);
-  res.render("encode.ejs", { result: encodedStr });
-  res.send(encodedStr);
-});
-
-app.get("/logout", (req, res) => {
-  return res
-    .clearCookie("token")
-    .status(200)
-    .redirect("/");
-});
-
+// if user authenticated - return encoded user input 
+app.post("/encode", authorization, (req, res) => {
+    const encodedString = encoder(req.body.userInput);
+    res.send(encodedString);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
