7e8934914dfe7f0bb13d4ea93dabd64f05a8674b
J-Filip <filip.jugkala@yahoo.com>
2023-02-21 23:35:22 +0100

    removed console log

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,5 +1,4 @@
 module.exports = function authorization (req, res, next) {
-  console.log(req.cookies);
   const token = req.cookies.token;
   if (token !== 'xyz0987654321') {
     return res.sendStatus(401)
