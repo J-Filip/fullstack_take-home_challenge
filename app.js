d7a7648b3c8ab581216932b1ad4532f1c8c3bb63
J-Filip <filip.jugkala@yahoo.com>
2023-02-21 23:46:48 +0100

    Added comments, removed sending encoded string twice

--- app.js
+++ app.js
@@ -2,45 +2,45 @@ const express = require('express')
 let bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser");
 
-const login = require('./login')
+const login = require('./middleware/login')
 const authorization = require("./middleware/authorization");
 const encoder= require("./encoder");
-
 
 const app = express()
 const port = 3000
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
 app.set("view engine", "ejs");
 
-
 // routes
 
+// login view
 app.get('/', (req, res) => {
       res.render('login.ejs')
 })
 
 app.post('/login', login)
 
+// encoder view
 app.get("/encode", authorization, (req, res) => {
   res.render("encode.ejs");
 });
 
-// define a new endpoint that accepts a string
+// define a new endpoint that accepts a string, runs it through encoder and returns the value 
 app.post("/encode", (req, res) => {
   const encodedStr = encoder(req.body.userInput);
   res.render("encode.ejs", { result: encodedStr });
-  res.send(encodedStr);
 });
 
+// logout 
 app.get("/logout", (req, res) => {
   return res
     .clearCookie("token")
     .status(200)
     .redirect("/");
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
